e28c047ec10440793b5242040ae0f99486803c75
Kumud Joshi <kumudj23@gmail.com>
2019-08-14 17:11:13 -0400

    outlining service worker

--- service-worker.js
+++ service-worker.js
@@ -1,1 +1,15 @@
-self.addEventListener('fetch', (event) => {});
+self.addEventListener('install', (e) => {
+  console.log('[ServiceWorker] Installed');
+
+  self.skipWaiting();
+
+  e.waitUntil(
+    caches.open(precacheName).then()
+  )
+
+  }
+)
+
+self.addEventListener('fetch', (e) => {
+  
+});
